9afc6f6f0956655362eaff527fe155313fddb505
mengruitu <160701736+mengruitu@users.noreply.github.com>
2024-02-21 14:36:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'IULsCqUpxp2zPdHJoYIuByKFgB2ro0_8q1DWe4O9oBk',
+      id: 'osSNk6ROXgifbvsezmzT8S3L9xLM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '12-11',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
